8ff7a7d82c9be7e97e6af1f28f996991fab39351
xiaminxi <1044485881@qq.com>
2019-12-16 19:53:20 +0800

    修改入口文件

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
  * @Author: 夏民喜
  * @Date: 2019-11-13 20:31:26
  * @LastEditors: 夏民喜
- * @LastEditTime: 2019-12-05 11:35:12
- * @Description: 请输入文件说明
+ * @LastEditTime: 2019-12-16 19:52:34
+ * @Description: 入口文件
  */
 import React from "react"
 import ReactDOM from "react-dom"
